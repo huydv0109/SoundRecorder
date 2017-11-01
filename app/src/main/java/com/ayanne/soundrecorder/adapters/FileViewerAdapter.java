58ce03ee256a30a2904bc73f9cec6c897fc06ca3
huydv0109 <huydv0109@gmail.com>
2017-11-01 17:18:42 +0700

    add call recorder library

--- app/src/main/java/com/ayanne/soundrecorder/adapters/FileViewerAdapter.java
+++ app/src/main/java/com/ayanne/soundrecorder/adapters/FileViewerAdapter.java
@@ -235,7 +235,7 @@ public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.Re
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         try {
-                            String value = input.getText().toString().trim() + ".mp4";
+                            String value = input.getText().toString().trim() + ".arm";
                             rename(position, value);
 
                         } catch (Exception e) {
